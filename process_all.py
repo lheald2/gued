49f256ea55da7a02525f9f49418f29525e2c6cf0
lheald2 <lheald2@unl.edu>
2024-07-18 18:35:54 -0500

    Updating process_all.py file to include a center smoothing algorithm and testing other components

--- process_all.py
+++ process_all.py
@@ -4,6 +4,7 @@ import glob
 import matplotlib.pyplot as plt
 from datetime import date
 import time
+from scipy.signal import savgol_filter
 
 # new code 
 import gued
@@ -29,7 +30,8 @@ if __name__ == "__main__":
     today = date.today()
     print(today)
 
-    file_name = f"{exp_label}_{today}.h5"
+    file_path = "C:\\Users\\laure\\OneDrive - University of Nebraska-Lincoln\\Documents\\Centurion Lab\\nitrophenyl code\\20180823\\"
+    file_name = file_path + f"{exp_label}_{today}.h5"
     print(f"writing data to {file_name}")
     group_name = "s1"
     #group_name = "s4"
@@ -74,38 +76,64 @@ if __name__ == "__main__":
         print(f"Remasking Data with np.nan value for files {groups[i]} to {groups[i+1]}")
         data_array = gued.apply_mask(data_array, fill_value=np.nan, plot=False)
 
-        print(f"Removing Radial Outliers for files {groups[i]} to {groups[i+1]}")
-        
-        cleaned_data, pct_outliers = gued.remove_radial_outliers_pool(data_array, centers, plot=False, return_pct=True)
-
-        data_array = cleaned_data 
-        del cleaned_data
-
         print(f"Median Filtering for files {groups[i]} to {groups[i+1]}")
         clean_data = gued.median_filter_pool(data_array, plot=False)
 
         data_array = clean_data
         del clean_data
-        
-        print(f"Calculating the Azimuthal average and Normalizing for files {groups[i]} to {groups[i+1]}")
-        norm_data, norm_std = gued.get_azimuthal_average_pool(data_array, centers, normalize=True, plot=False)
 
         print(f"Saving Data for files {groups[i]} to {groups[i+1]} as run number {i}")
         # Make dictionary for saving to h5 file
 
         data_dictionary = {
             'clean_images': data_array,
-            'I': norm_data, 
             'stage_positions': stage_positions, 
             'centers': centers, 
             'total_counts': counts, 
-            'percent_xrays': pct_xrays, 
-            'percent_outliers': pct_outliers}
+            'percent_xrays': pct_xrays}
         
-        data_note = "Using center value for each image"
+        data_note = "Testing new processing with smoothed centers"
         gued.save_data(file_name, group_name, i, data_dictionary, group_note=data_note)
         del data_array, stage_positions, file_numbers, counts, counts_mean, counts_std
 
     stop = time.perf_counter()
 
-    print(f"Finished processing {int(groups[-1])-(groups[0])} in {(stop-start)/60} minutes")
+    print(f"Finished pre-processing {int(groups[-1])-(groups[0])} in {(stop-start)/60} minutes")
+
+    variable_names = ["centers", "clean_images"]
+    
+    combined_data = gued.read_combined_data(file_name, group_name, variable_names, run_numbers='all')
+    centers_x = combined_data['centers'][:,0]
+    centers_y = combined_data['centers'][:,1]
+
+    print(f"Smoothing center values using Savitzky-Golay filter")
+    # Apply Savitzky-Golay filter
+    window_size = 101  # Choose an odd number for the window size
+    poly_order = 1  # Choose the polynomial order
+    smoothed_x = savgol_filter(centers_x, window_size, poly_order)
+    smoothed_y = savgol_filter(centers_y, window_size, poly_order)
+    smoothed_centers = list(zip(smoothed_x, smoothed_y))
+
+    for i in range(len(groups)-1):
+        data_array = combined_data["clean_images"][groups[i]:groups[i+1]]
+        centers = smoothed_centers[groups[i]:groups[i+1]]
+
+        print(f"Removing Radial Outliers for files {groups[i]} to {groups[i+1]}")
+        cleaned_data, pct_outliers = gued.remove_radial_outliers_pool(data_array, centers, plot=False, return_pct=True)
+
+        data_array = cleaned_data 
+        del cleaned_data
+
+        print(f"Calculating the Azimuthal average and Normalizing for files {groups[i]} to {groups[i+1]}")
+        norm_data, norm_std = gued.get_azimuthal_average_pool(data_array, centers, normalize=True, plot=False)
+
+        data_dictionary = {"percent_outliers": pct_outliers, 
+                           "new_centers": centers,
+                           "I": norm_data}
+        
+        gued.add_to_h5(file_name, group_name, data_dictionary, i)
+        del norm_data, norm_std, data_array, centers, pct_outliers
+        print(f"Done post processing files {groups[i]} to {groups[i+1]}")
+    
+    stop2 = time.perf_counter()
+    print(f"Finished all processing {int(groups[-1])-(groups[0])} in {(stop2-start)/60} minutes")
